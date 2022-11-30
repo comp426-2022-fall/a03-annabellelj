d35dc0824118742f860fe80befae76315d98545f
Annabelle Jiang <annlin@email.unc.edu>
2022-11-29 23:28:45 -0500

    Added log file.

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import minimist from "minimist";
-import {roll} from "../lib/roll.js";
+import minimist from 'minimist';
+import {roll} from '../lib/roll.js';
 const args = minimist(process.argv.slice(2));
 const sides = args.slides || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
-console.log(JSON.stringify(roll(sides, dice, rolls)));
+console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
